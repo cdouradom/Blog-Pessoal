55369a0fbd5a5dea6cb31dc77c599c29d5b56928
Cintia_Dourado <cdouradom@gmail.com>
2025-10-14 14:22:33 -0300

    :books: Atividade 03-CRUD - Blog Pessoal - parte 02:sparkles:

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,15 +1,25 @@
 package com.generation.blogpessoal.controller; // Define o pacote onde a classe está localizada
 
 import java.util.List; // Importa a classe List para trabalhar com listas
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired; // Importa a anotação Autowired para injeção de dependências
+import org.springframework.http.HttpStatus; // Importa a enumeração HttpStatus para definir códigos de status HTTP
 import org.springframework.http.ResponseEntity; // Importa a classe ResponseEntity para manipular respostas HTTP
 import org.springframework.web.bind.annotation.CrossOrigin; // Importa a anotação CrossOrigin para configurar CORS
+import org.springframework.web.bind.annotation.DeleteMapping; // Importa a anotação DeleteMapping para mapear requisições DELETE
 import org.springframework.web.bind.annotation.GetMapping; // Importa a anotação GetMapping para mapear requisições GET
 import org.springframework.web.bind.annotation.PathVariable; //	 Importa a anotação PathVariable para extrair variáveis da URL
+import org.springframework.web.bind.annotation.PostMapping; // Importa a anotação PostMapping para mapear requisições POST
+import org.springframework.web.bind.annotation.PutMapping; // Importa a anotação PutMapping para mapear requisições PUT
+import org.springframework.web.bind.annotation.RequestBody; // Importa a anotação RequestBody para extrair o corpo da requisição
 import org.springframework.web.bind.annotation.RequestMapping; // Importa a anotação RequestMapping para mapear URLs
+import org.springframework.web.bind.annotation.ResponseStatus; // Importa a anotação ResponseStatus para definir o status de resposta padrão
 import org.springframework.web.bind.annotation.RestController; // Importa a anotação RestController para definir um controlador REST
+import org.springframework.web.server.ResponseStatusException; // Importa a classe ResponseStatusException para lançar exceções com status HTTP
 import com.generation.blogpessoal.model.Postagem; // Importa a classe Postagem da model
 import com.generation.blogpessoal.repository.PostagemRepository; // Importa a interface PostagemRepository da repository
+import jakarta.validation.Valid; // Importa a anotação Valid para validação de dados
 
 // Define que essa classe é um controlador REST
 @RestController
@@ -24,13 +34,50 @@ public class PostagemController { // Define a classe PostagemController
 	public ResponseEntity<List<Postagem>> getAll() { // Define o método getAll que retorna uma lista de Postagem dentro de um ResponseEntity
 		return ResponseEntity.ok(postagemRepository.findAll()); // Retorna a lista de postagens com status 200 (OK)
 
-		// SELECT * FROM db_blogpessoal.tb_postagens;
+		// equivalente ao SELECT * FROM db_blogpessoal.tb_postagens;
 	}
 
 	@GetMapping("/{id}") // Mapeia requisições GET com um ID na URL para esse método
 	public ResponseEntity<Postagem> getById(@PathVariable Long id) { // Extrai o ID da URL e define o método getById que retorna
-		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta)) // uma Postagem dentro de um ResponseEntity
-				.orElse(ResponseEntity.notFound().build()); // Retorna a postagem com status 200 (OK) ou 404 (Not Found) se não encontrada
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta)) // uma Postagem dentro de um ResponseEntity
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); // Retorna a postagem com status 200 (OK) ou 404 (Not Found) se não encontrada
+		// equivalente ao SELECT * FROM db_blogpessoal.tb_postagens WHERE id = ?;	
 	}
-	// SELECT * FROM db_blogpessoal.tb_postagens WHERE id = id;
+	
+	@GetMapping("/titulo/{titulo}") // Mapeia requisições GET com um título na URL para esse método
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) { // Extrai o título da URL e define o método getByTitulo que retorna uma lista de Postagem dentro de um ResponseEntity
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo)); // Retorna a lista de postagens que contém o título, ignorando maiúsculas e minúsculas, com status 200 (OK)
+		
+		// equivalente ao SELECT * FROM db_blogpessoal.tb_postagens WHERE titulo LIKE "%titulo%";
+	}
+	
+	@PostMapping // Mapeia requisições POST para esse método
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) { // Define o método post que recebe uma Postagem no corpo da requisição e retorna uma Postagem dentro de um ResponseEntity
+		postagem.setId(null); // Garante que o ID seja nulo para criar uma nova postagem
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem)); // Salva a postagem e retorna com status 201 (Created)
+		
+		// equivalente ao INSERT INTO tb_postagens (data, texto, titulo) VALUES (current_timestamp(), 'Texto', 'Titulo');
+	}
+	
+	@PutMapping // Mapeia requisições PUT para esse método
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) { //	 Define o método put que recebe uma Postagem no corpo da requisição e retorna uma Postagem dentro de um ResponseEntity
+		return postagemRepository.findById(postagem.getId()) // Verifica se a postagem com o ID fornecido existe
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem))) // Se existir, atualiza a postagem e retorna com status 200 (OK)
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); // Se não existir, retorna status 404 (Not Found)
+
+		// equivalente ao UPDATE tb_postagens SET data = current_timestamp(), texto = 'Texto', titulo = 'Titulo' WHERE id = ?;
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT) // Define o status de resposta como 204 (No Content)
+	@DeleteMapping("/{id}") // Mapeia requisições DELETE com um ID na URL para esse método
+	public void delete(@PathVariable Long id) { // Extrai o ID da URL e define o método delete que não retorna nada
+        Optional<Postagem> postagem = postagemRepository.findById(id); // Ver
+        if (postagem.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND); // Se a postagem não existir, lança uma exceção com status 404 (Not Found)
+        postagemRepository.deleteById(id); // Se existir, deleta a postagem pelo ID
+        
+        // equivalente ao DELETE FROM tb_postagens WHERE id = ?;
+    }
 }
+
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,17 @@
 package com.generation.blogpessoal.repository; // Define o pacote onde a classe está localizada
 
+import java.util.List; // Importa a classe List para trabalhar com listas
+
 import org.springframework.data.jpa.repository.JpaRepository; // Importa a interface JpaRepository do Spring Data JPA
+import org.springframework.data.repository.query.Param; // Importa a anotação Param para definir parâmetros em consultas
 
 import com.generation.blogpessoal.model.Postagem; // Importa a classe Postagem da model
 
 //Com qual model (tabela) essa Interface irá trabalhar, Long como a chave primária, no nosso caso é o id
 // JpaRepository já possui vários métodos prontos para serem usados (CRUD)
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{ // Define a interface PostagemRepository que estende JpaRepository 
+	
+	// Query Methods
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo); // Método para buscar postagens por título, ignorando maiúsculas e minúsculas
 	
 }
